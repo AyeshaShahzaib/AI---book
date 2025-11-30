ed4bbaf85143b683d4733a9712911e333cbb8807
AyeshaShahzaib <AyeshaShahzaib@users.noreply.github.com>
2025-11-30 09:03:55 +0000

    deploy: b1fd6e6c6211a00f5bec2aa616fd99c298d0612c

---
+++ assets/js/34772b01.409b3c67.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[3621],{9891:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/gemini-book/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.2c95c495.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkbook=globalThis.webpackChunkbook||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/gemini-book/blog","blogTitle":"Blog","authorsListPath":"/gemini-book/blog/authors"}')}}]);
